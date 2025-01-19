773bf2491091700350821cf9d55553aa19e6a4e8
mariemabdelsalam <mariemabdelsalam61@gmail.com>
2025-01-19 15:22:06 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+let myInput = document.getElementById('date')
+let myBtn = document.querySelector('button')
+let myelem = document.querySelector('p')
+// console.log(myInput, myBtn, myelem);
+
+myBtn.addEventListener('click', function () {
+    console.log('kjjhhh');
+    calcAge()
+})
+
+function calcAge() {
+    let today = new Date()
+    //    console.log(today);
+    let value = myInput.value
+    //    console.log(value);
+    let userDate = new Date(value)
+    //    console.log(userDate);
+    let year = today.getFullYear() - userDate.getFullYear()
+    // console.log(year);
+    let month = today.getMonth() - userDate.getMonth()
+    console.log(month);
+    let day = today.getDate() - userDate.getDate()
+    if (!value) {
+        myelem.innerHTML = `please select date`
+        return;
+    }
+    if (userDate > today) {
+        myelem.innerHTML = `invalid date`
+        return;
+    }
+    if (month < 0) {
+        year--;
+        month += 12;
+    }
+    if (day < 0) {
+        month--;
+        let lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 0)
+        day += lastMonth.getDate()
+    }
+    myelem.innerHTML = `you are ${year} years , ${month} months and ${day} days old`
+}
